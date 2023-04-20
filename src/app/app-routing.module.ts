9a82f6302d343450dd2f31c5a906c39e1eb03e53
KARABOU Manzama <manzamakarabou@gmail.com>
2023-04-20 16:46:25 +0000

    component patient

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisplayPatientsComponent } from './patients/display-patients/display-patients.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', component: DisplayPatientsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
